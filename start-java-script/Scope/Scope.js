30a30eef7170ec2d35a60b567da60370c73decf9
MD ASHIK MAHMUD <ashik.swfu@outlook.com>
2022-06-20 15:11:18 +0800

    note

---
+++ start-java-script/Scope/Scope.js
@@ -0,0 +1,15 @@
+/*
+JavaScript has 3 types of scope
+
+    1. Block scope
+        create with let and const
+    2. Function scope
+        create a variable insive a funcion
+        Variables declared with var, let and const are quite similar when declared inside a function.
+    3. Global scope
+        Automatically Global
+        If you assign a value to a variable that has not been declared, it will automatically become a GLOBAL variable.
+
+
+
+*/
